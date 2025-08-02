4f95064eb1780eee717b2999d51d75935eb9c235
SuwasG <suwasghale693@gmail.com>
2025-08-02 16:36:42 +0545

    user model created

---
+++ apps/users/models/user.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionMixin, BaseUserManager
+
+class  CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a regular User with the given email and password.
+        """
+        if not email:
+            raise ValueError("User must have an email address.")
+        email = self.normalize_email(email) # Normalize the email by lowercasing the domain part of the email.
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password) # Hashes the password.
+        user.save(using=self._db)  # save to the correct database.
+        # user.save()
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        # validating superuser flags.
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionMixin):
+    """
+    Custom user model that uses email instead of username.
+    """
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15)
+    date_of_birth = models.DateField(null= True, blank= True)
+    profile_image = models.URLField(null=True, blank=True) # cloudinary
+    profile_image = models.ImageField(upload_to='media/profile_images', null=True, blank=True) # local
+    
+    # Permissions & status
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    # Timestamps
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    # Link Manager
+    objects = CustomUserManager()
+
+    # Tell Django which field is used to log in
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    def __str__(self):
+        return self.email
+
