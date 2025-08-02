35ad4be0a779ffc3168e408a4a184027f057876b
SuwasG <suwasghale693@gmail.com>
2025-08-02 21:38:06 +0545

    admin on users app

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Address
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number', 'date_of_birth', 'profile_image')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')})
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address_type', 'city', 'country')
+    search_fields = ('user__email', 'city', 'country')
---
+++ apps/users/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import CustomUser
+from .address import Address
+
+# settings.py: AUTH_USER_MODEL = 'users.CustomUser'
