eb2812366de797b34bf16817bc1338681d5a7539
SuwasG <suwasghale693@gmail.com>
2025-08-07 22:06:34 +0545

    updated urls for users

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register_view, login_view, profile_view
+from .views import register_view, login_view, profile_view, profile_update_view
 
 urlpatterns=[
    
     path('register', register_view, name = 'register'),
     path('login',login_view, name='login' ),
-    path('profile', profile_view, name = 'profile')
+    path('profile', profile_view, name = 'profile'),
+    path('update', profile_update_view, name = 'profile_update'),
 ]
