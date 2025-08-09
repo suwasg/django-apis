d669f9281f72bf5fbdbe8921c02ab35a71f2092d
SuwasG <suwasghale693@gmail.com>
2025-08-09 06:30:06 +0545

    login required mixins created

--- apps/common/mixins.py
+++ apps/common/mixins.py
@@ -12,3 +12,15 @@ class SuccessMessageMixin:
        if self.success_message:
               messages.success(self.request, self.success_message)
        return response
+
+class LoginRequiredMixin:
+    """
+    A mixin that requires the user to be logged in to access the view.
+    If the user is not authenticated, they will be redirected to the login page.
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, "You must be logged in to access this page.")
+            return redirect('user/login')  # Redirect to the login page if not authenticated
+        return super().dispatch(request, *args, **kwargs)
+        
